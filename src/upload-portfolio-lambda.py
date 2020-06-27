c2f8a71b25359fd76eaadf3d8dbea69b017e52c9
Tim Wang <wangtian60d@gmail.com>
2020-06-27 18:50:13 -0400

    User bucket name and object key from codepipeline

--- src/upload-portfolio-lambda.py
+++ src/upload-portfolio-lambda.py
@@ -4,17 +4,36 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
 
-portfolio_bucket = s3.Bucket('port.timwang.space')
-build_bucket = s3.Bucket('portbuild.timwang.space')
+    location = {
+        "bucketName": 'portbuild.timwang.space',
+        "objectKey": 'portbuild.zip'
+    }
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portbuild.zip', portfolio_zip)
+    job = event.get("CodePipeline.job")
+    if job:
+        for artifact in job["data"]["inputArtifacts"]:
+            if artifact["name"] == "BuildArtifact":
+                location = artifact["location"]["s3Location"]
 
-with zipfile.ZipFile(portfolio_zip) as myZip:
-    for file in myZip.namelist():
-        obj = myZip.open(file)
-        portfolio_bucket.upload_fileobj(obj, file,
-          ExtraArgs={'ContentType': mimetypes.guess_type(file)[0]})
-        portfolio_bucket.Object(file).Acl().put(ACL='public-read')
+    print "Building portfolio from " + str(location)
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+
+    portfolio_bucket = s3.Bucket('port.timwang.space')
+    build_bucket = s3.Bucket(location["bucketName"])
+
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myZip:
+        for file in myZip.namelist():
+            obj = myZip.open(file)
+            portfolio_bucket.upload_fileobj(obj, file,
+              ExtraArgs={'ContentType': mimetypes.guess_type(file)[0]})
+            portfolio_bucket.Object(file).Acl().put(ACL='public-read')
+
+    if job:
+        codepipeline = boto3.client('codepipeline')
+        codepipeline.put_job_success_result(jobId=jon["id"])
+    return 'Success'
