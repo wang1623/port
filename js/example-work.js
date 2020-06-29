fe6ad159afdec5b3c102bdce9e165efd2c80fb2d
Tim Wang <wangtian60d@gmail.com>
2020-06-29 11:38:35 -0400

    add more react interactive code

--- js/example-work.js
+++ js/example-work.js
@@ -1,28 +1,74 @@
 import React from 'react';
+import ExampleWorkModal from './example-work-modal';
 
 class ExampleWork extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    //the value of this.state is always a javascript object
+    //we want to think what states we want to manage.
+    //To change the state, we need to use event handler
+    this.state = {
+      'modalOpen': false,
+      'selectedExample': this.props.work[0]
+    }
+
+    //bind() in constructor gives these functions access to our object.
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal(event, example){
+    this.setState({
+      'modalOpen': true,
+      'selectedExample': example
+    });
+  }
+
+  closeModal(event){
+    this.setState({
+      'modalOpen': false
+    });
+  }
+
+  //we can only return one section in the return(). So we use <span> to wrap them.
+  /* the state modalOpen is managed by the parent component 'ExampleWork' */
   render(){
     return (
-      <section className="section section--alignCentered section--description">
+      <span>
+        <section className="section section--alignCentered section--description">
 
-        {this.props.work.map((example, index) => {
-            return(
-              //refer other component in JSX
-              <ExampleWorkBubble example={example} key={index}/>
-            )
-          })
-        }
+          {this.props.work.map((example, index) => {
+              return(
+                /* refer other component in JSX */
+                <ExampleWorkBubble example={example} key={index}
+                  openModal={this.openModal}/>
+              )
+            })
+          }
 
-      </section>
+        </section>
+
+      
+        <ExampleWorkModal example={this.state.selectedExample}
+          open={this.state.modalOpen} closeModal={this.closeModal}/>
+      </span>
     )
   }
 }
 
+//since each component is a standalone object, the ExampleWorkBubble does not have access to modalOpen/modalClose functions.
+//so we should pass these function into the component as prop.
 class ExampleWorkBubble extends React.Component {
   render() {
     let example = this.props.example;
     return (
-      <div className="section__exampleWrapper">
+      /* The browser will call this openModal as soon as this <div> is clicked
+          onClick needs a function. The function delegates to openModal.
+          ExampleWorkBubble should know which example it represent so we pass the example to openModal function.
+      */
+      <div className="section__exampleWrapper" onClick={(event) => this.props.openModal(event, example)}>
         <div className="section__example">
           <img alt= { example.image.description }
                className="section__exampleImage"
--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ const myWork = [
   //3 javascript object literals
   {
     'title' : "Work Example",
+    'href': "https://example.com",
+    'description': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image' : {
       'description': "example screenshot of a project involving code",
       'src': "images/example1.png",
@@ -14,6 +16,8 @@ const myWork = [
   },
   {
     'title' : "Portfolio Boilerplate",
+    'href': "https://example.com",
+    'description': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image' : {
       'description': "example screenshot of a project involving chemistry",
       'src': "images/example2.png",
@@ -22,6 +26,8 @@ const myWork = [
   },
   {
     'title' : "Work Example",
+    'href': "https://example.com",
+    'description': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image' : {
       'description': "example screenshot of a project involving cats",
       'src': "images/example3.png",
